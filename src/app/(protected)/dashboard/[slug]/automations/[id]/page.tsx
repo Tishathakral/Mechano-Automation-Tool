ee25fa30f3f2c659b98c882a16680425406a42b3
Tishathakral <thakraltisha@gmail.com>
2025-01-05 01:39:30 +0530

    feat: trigger

--- src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -1,6 +1,7 @@
-
+import Trigger from "@/components/global/automations/trigger";
 import AutomationsBreadCrumb from "@/components/global/bread-crumbs/automations";
 import React from "react";
+import { PiSealWarning } from "react-icons/pi";
 
 type Props = {
   params: {
@@ -13,10 +14,11 @@ const Page = async ({ params }: Props) => {
     <div className=" flex flex-col items-center gap-y-20">
       <AutomationsBreadCrumb id={params.id} />
       <div className="w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3">
-        <div className="flex gap-x-2">
-          {/* <Warning /> */}
+        <div className="flex gap-x-2 items-center">
+         <PiSealWarning className="h-5 w-5 text-indigo-600"/>
           When...
         </div>
+        <Trigger/>
       </div>
     </div>
   );
---
+++ src/components/global/automations/trigger/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Trigger = (props: Props) => {
+  return (
+    <div>Trigger</div>
+  )
+}
+
+export default Trigger
