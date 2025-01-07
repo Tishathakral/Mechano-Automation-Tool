a689e8d5f5182036b5c78e28f141971a97c74031
Tishathakral <thakraltisha@gmail.com>
2025-01-07 22:36:36 +0530

    fix: param issue

--- src/actions/user/queries.ts
+++ src/actions/user/queries.ts
@@ -1,45 +1,45 @@
-"use server"
+"use server";
 
-import { client } from "@/lib/prisma"
+import { client } from "@/lib/prisma";
 
 export const findUser = async (clerkId: string) => {
-    return await client.user.findUnique({
-        where: {
-            clerkId
+  return await client.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      subscription: true,
+      integrations: {
+        select: {
+          id: true,
+          token: true,
+          expiresAt: true,
+          name: true,
         },
-        include: {
-            subscription: true,
-            integrations:{
-                select:{
-                    id:true,
-                    token:true,
-                    expiresAt:true,
-                    name:true
-                },
-            },
-        },
-    })
-}
+      },
+    },
+  });
+};
 
 export const createUser = async (
-    clerkId: string,
-    email: string,
-    firstname: string,
-    lastname: string,
+  clerkId: string,
+  firstname: string,
+  lastname: string,
+  email: string
 ) => {
-    return await client.user.create({
-        data: {
-            clerkId,
-            email,
-            firstname,
-            lastname,
-            subscription:{
-                create:{},
-            },
-        },
-    select:{
-        firstname:true,
-        lastname:true,
+  return await client.user.create({
+    data: {
+      clerkId,
+      firstname,
+      lastname,
+      email,
+      subscription: {
+        create: {},
+      },
     },
-    })
-}
+    select: {
+      firstname: true,
+      lastname: true,
+    },
+  });
+};
