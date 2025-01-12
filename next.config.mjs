38789b675210923d93f01bf02cc0fb028dc44f17
Tishathakral <thakraltisha@gmail.com>
2025-01-12 23:38:27 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'instagram.fdel8-2.fna.fbcdn.net',
+            },
+            {
+                protocol: 'https',
+                hostname: 'scontent.cdninstagram.com',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
